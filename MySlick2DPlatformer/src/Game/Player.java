0e73ed1b80d6120586f1a3521b213444bdbabb8e
Ethan McWhirter <ethanmcwhirter@gmail.com>
2017-01-23 15:48:36 -0600

    Second

---
+++ MySlick2DPlatformer/src/Game/Player.java
@@ -0,0 +1,82 @@
+package Game;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
+
+public class Player {
+	private float x,y,velX,velY;
+	private Shape bounds;
+	private float boost,trail;
+	
+	
+	
+	public Player()
+	{
+		bounds = new Rectangle(x,y,32,8);
+	}
+	
+	public Player(int x,int y)
+	{
+		this.x = x;
+		this.y = y;
+		bounds = new Rectangle(x,y,32,8);
+		boost = 100;
+		trail = 100;
+	}
+
+	
+	public void move(double angle)
+	{
+		setVelX(3*(float) Math.cos(angle));
+		setVelY(3*(float) Math.sin(angle));
+		bounds = new Rectangle(x,y,32,8);
+		bounds =  bounds.transform(Transform.createRotateTransform((float) angle,x,y));
+	}
+	
+	public void update()
+	{
+		x+=velX;
+		y+=velY;
+	}
+	
+	public void render(Graphics g)
+	{
+		
+		g.fill(bounds);
+		
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getVelX() {
+		return velX;
+	}
+
+	public void setVelX(float velX) {
+		this.velX = velX;
+	}
+
+	public float getVelY() {
+		return velY;
+	}
+
+	public void setVelY(float velY) {
+		this.velY = velY;
+	}
+}
---
+++ MySlick2DPlatformer/src/Game/Trail.java
@@ -0,0 +1,19 @@
+package Game;
+
+import org.newdawn.slick.geom.Rectangle;
+
+public class Trail {
+	private float x,y;
+	private Rectangle bounds;
+	
+	public Trail()
+	{
+		
+	}
+	public Trail(float x, float y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+}
